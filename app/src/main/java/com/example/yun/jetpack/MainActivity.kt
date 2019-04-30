16b75f65555782aa8800c57983a73af244947336
yun <yyjan219@gmail.com>
2019-04-30 17:23:07 +0900

    feat: update gradle

--- app/src/main/java/com/example/yun/jetpack/MainActivity.kt
+++ app/src/main/java/com/example/yun/jetpack/MainActivity.kt
@@ -2,11 +2,18 @@ package com.example.yun.jetpack
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.yun.jetpack.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
     }
+
 }
---
+++ app/src/main/java/com/example/yun/jetpack/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.yun.jetpack.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
